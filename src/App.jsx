4b5d9f3fa403e76b080f60b73b6308372702b7ca
Miguel Godoy <43613535+mgodoydiaz@users.noreply.github.com>
2026-01-02 00:39:10 +0000

    first version

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+    return (
+        <div>
+            <h1>Hola desde mi iPad</h1>
+            <p> Esta es una de mis primeras páginas en React  </p>
+        </div>
+    );
+}
+
+export default App;
